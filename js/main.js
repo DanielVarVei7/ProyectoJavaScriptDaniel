9e9b89f960c49e15d50c7c22fd44889d2695d6bc
Alfa-666 <yosetaguilar2008@cesanrafael.org>
2024-11-19 14:18:24 -0400

    ComidaPicante

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let mensaje="Hola Mndo";
+console.log (mensaje);
+let numero1=80;
+let numero2=44;
+let numero3=10;
+let resultadosuma=Numero1+Numero2+Numero3;
+console.log resultadosuma
+
+let resultadoresta=Numero1-Numero2-Numero3;
+console.log resultadoresta
+
+let resultadomulti=Numero1*Numero2*Numero3;
+console.log resultadomulti
+
+let resultadodivi=Numero1/Numero2/Numero3;
+console.log resultadodivi
+
+
+function sumar (){
+    let numero1=80;
+let numero2=44;
+let numero3=10;
+let resultadosuma=Numero1+Numero2+Numero3;
+
+}
+alert("El resultado es"resultadosuma)
+
+function sumar (){
+    let numero1=80;
+let numero2=44;
+let numero3=10;
+let resultadosuma=Numero1-Numero2-Numero3;
+
+}
+alert("El resultado es"resultadosresta)
+
+function sumar (){
+    let numero1=80;
+let numero2=44;
+let numero3=10;
+let resultadosuma=Numero1*Numero2*Numero3;
+
+}
+alert("El resultado es"resultadomultiplicacion)
+
+function sumar (){
+    let numero1=80;
+let numero2=44;
+let numero3=10;
+let resultadosuma=Numero1/Numero2/Numero3;
+
+}
+alert("El resultado es"resultadodivision)
